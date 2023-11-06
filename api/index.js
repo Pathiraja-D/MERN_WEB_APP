b1039d61177162155436c558d2674538980952b1
Dinuk Pathiraja <139424859+Pathiraja-D@users.noreply.github.com>
2023-11-06 19:58:58 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect("mongodb+srv://pathiraja:pathiraja%40123@mern-estate.gxhpbez.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('Connected to mongodb!');
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 app.listen(3000,()=>{
     console.log('Server is running');
 });
+
